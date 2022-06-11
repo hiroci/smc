972d62b7349482bd309beb83835ac618df5db3fe
Hiro Ishida <hiroci260500@gmail.com>
2022-06-11 19:01:06 -0300

    first commit

--- EX-2.c
+++ EX-2.c
@@ -4,14 +4,18 @@
 int main(void)
 {
     char array[100];
+    char arrayUpper[100];
+    char arrayLower[100];
     
     printf("String: ");
     scanf("%[^\n]s", &array);
 
     for(int i = 0; i < 100; i++)
     {
-        array[i] = toupper( array[i]);
+        arrayUpper[i] = toupper( array[i]);
+        arrayLower[i] = tolower( array[i]);
     }
 
-    printf("String: %s",array);
+    printf("String uppercase: %s\n",arrayUpper);
+    printf("String lowercase: %s",arrayLower);
 }
